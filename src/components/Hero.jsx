77e054c8d18c6f8f4ffd19afc0b1256ab806dc30
shafeeq8904 <shafeeq8904@gmail.com>
2024-09-25 10:37:10 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+function Hero(){
+    return (
+        <h1 className="text-center">
+            hero
+        </h1>
+    )
+}
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faS ,faA} from '@fortawesome/free-solid-svg-icons'; 
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+
+function Navbar() {
+    return (
+        <nav className='mb-20 flex items-center justify-between py-6 '>
+            <div className='flex flex-shrink-0 items-center text-gray-400 gap-1'>
+                <FontAwesomeIcon className='text-3xl'  icon={faS} />
+                <FontAwesomeIcon icon={faA} />
+            </div>
+
+            <div className='m-8 flex gap-4 text-2xl justify-center items-center  text-gray-400'>
+                <FaLinkedin/>
+                <FaGithub/>
+                <FaInstagram/>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
