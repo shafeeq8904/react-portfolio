4eabbfef2780b2c81888135ed4fa53373b9660a4
shafeeq8904 <shafeeq8904@gmail.com>
2024-09-26 10:03:40 +0530

    Add assets folder with images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import {HERO_CONTENT} from "../constants"
-import Profile from '../assets/Profile.jpg'; 
+import Profile from '../assets/profile.jpg'; 
 import { motion } from "framer-motion"
 
 const container = (delay) => ({
